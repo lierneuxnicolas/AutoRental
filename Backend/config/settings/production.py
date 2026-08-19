d354101f1801c6339c14419834bd8ddb919fec98
lierneuxnicolas <lierneuxnicolas@outlook.com>
2026-08-19 15:46:31 +0200

    Deploy Django backend to Azure and connect MySQL

--- Backend/config/settings/production.py
+++ Backend/config/settings/production.py
@@ -21,3 +21,30 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+DATABASES["default"]["OPTIONS"] = {
+    **DATABASES["default"].get("OPTIONS", {}),
+    "ssl": {
+        "ca": os.getenv("DB_SSL_CA", "/etc/ssl/certs/ca-certificates.crt"),
+    },
+}
+
+AZURE_ACCOUNT_NAME = os.getenv("AZURE_ACCOUNT_NAME", "")
+AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY", "")
+AZURE_CONTAINER = os.getenv("AZURE_CONTAINER", "media")
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.azure_storage.AzureStorage",
+        "OPTIONS": {
+            "account_name": AZURE_ACCOUNT_NAME,
+            "account_key": AZURE_ACCOUNT_KEY,
+            "azure_container": AZURE_CONTAINER,
+            "overwrite_files": False,
+            "expiration_secs": 3600,
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
